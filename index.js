1a70bc15363e21e4e92bd9df9858b81231ae2508
Dave Degeatano <davey.dee33@gmail.com>
2017-04-25 16:18:16 +1000

    about to start introducing EJS template engine

--- index.js
+++ index.js
@@ -7,23 +7,40 @@ var app = express();
 // middleware example.
 var logger = function(req, res, next) {
     console.log('Logging...');
-    console.log(__dirname)
     next();
 }
 
 app.use(logger);
 
 
-
-// using the Body Parser - middleware.  Look up this stuff in the documentation for body parser
-app.use(bodyParser.json); // to parse JSON in HTTP body response
+// Middleware - uusing the Body Parser.  Look up this stuff in the documentation for body parser
+app.use(bodyParser.json()); // to parse JSON in HTTP body response
 app.use(bodyParser.urlencoded({extended: false}));  // look this up in the documentation.
 
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));  // eg.  app.use(express.static('/blah/public'))
+// this would end up serving (public/index.htm), since this comes before the later statement to serve "/" with something else.
+
+// testing parsing JSON object to the browser.
+var people = [
+    {
+        name: 'One',
+        age: 1
+    },
+    {
+        name: 'Two',
+        age: 2
+    },
+    {
+        name: 'Three',
+        age: 3
+    }
+]
+
 
 app.get('/', function(req, res){
-    res.send('Received your GET request...\n')
+    res.send('Received your GET request...\n')        // send raw text?
+    //res.json(people);
 })
 
 app.post('/', function(req, res){
